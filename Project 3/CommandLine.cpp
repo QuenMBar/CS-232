e6fb868eeb05514155f0999251be1f1f2df845df
Quentin Barnes <qmb2@gold30.cs.calvin.edu>
2019-03-29 21:12:19 -0400

    Proj 03 inc 1

--- Project 3/CommandLine.cpp
+++ Project 3/CommandLine.cpp
@@ -15,24 +15,57 @@ CommandLine::CommandLine(istream &in)
     int otherInc = 0;
     for (int i = 0; i < commandEntered.size(); i++)
     {
+        if (commandEntered.find(" ", otherInc) == i)
+        {
+            otherInc = i + 1;
+            argc++;
+        }
+        else if (commandEntered.find(" ", otherInc) == otherInc + 1)
+        {
+            otherInc++;
+        }
+    }
+    if (commandEntered.find(" ", commandEntered.size() - 1) != (commandEntered.size() - 1))
+    {
+        argc++;
+    }
+
+    argv = (char *)calloc(argc, 10);
+    otherInc = 0;
+    int otherOtherInc = 0;
+    for (int i = 0; i < commandEntered.size(); i++)
+    {
+        cout << "For " << i << ": ";
         if (commandEntered.find(" ", otherInc) == 0)
         {
             otherInc++;
         }
         else if (commandEntered.find(" ", otherInc) == i)
         {
+            cout << otherInc << ": " << i << "\n"
+                 << flush;
+            strcpy(&argv[otherOtherInc], commandEntered.substr(otherInc, i - otherInc).c_str());
+            cout << "Substrings: " << commandEntered.substr(otherInc, i - otherInc) << "\n"
+                 << flush;
             otherInc = i + 1;
-            argc++;
+            otherOtherInc++;
         }
     }
-    if (!(commandEntered.find(" ", commandEntered.size() - 1) == (commandEntered.size() - 1)))
+    if (commandEntered.find(" ", commandEntered.size() - 1) != (commandEntered.size() - 1))
     {
-        argc++;
+        strcpy(&argv[otherOtherInc], commandEntered.substr(otherInc, commandEntered.size() - 1).c_str());
+        cout << "Substrings: " << commandEntered.substr(otherInc, commandEntered.size() - 1) << "\n"
+             << flush;
     }
 }
 
 void CommandLine::printCommand()
 {
-    cout << commandEntered << ": " << argc << "\n"
-         << flush;
+    cout << "\n"
+         << argc << "\n\n";
+    for (int i = 0; i < argc; i++)
+    {
+        cout << argv[i] << "\n";
+    }
+    cout << flush;
 }
